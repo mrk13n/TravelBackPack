3857e2c584e414385b31cfc895253f240cae4e69
Nazar <naz466@yandex.ru>
2017-12-06 15:05:36 +0200

    commit

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+/**
+ * Created by Andriy on 10.03.2015.
+ */
+module.exports = function(grunt) {
+    //Налаштування збірки Grunt
+    var config = {
+        //Інформацію про проект з файлу package.json
+        pkg: grunt.file.readJSON('package.json'),
+
+        //Конфігурація для модуля browserify (перетворює require(..) в код
+        browserify:     {
+            //Загальні налаштування (grunt-browserify)
+            options:      {
+
+                //brfs замість fs.readFileSync вставляє вміст файлу
+                transform:  [ require('brfs') ],
+                browserifyOptions: {
+                    //Папка з корнем джерельних кодів javascript
+                    basedir: "Frontend/src/"
+                }
+            },
+
+            //Збірка з назвою піца
+            map: {
+                src:        'Frontend/src/map.js',
+                dest:       'Frontend/www/assets/js/map.js'
+            }
+        }
+    };
+
+    //Налаштування відстежування змін в проекті
+    var watchDebug = {
+        options: {
+            'no-beep': true
+        },
+        //Назва завдання будь-яка
+        scripts: {
+            //На зміни в яких файлах реагувати
+            files: ['Frontend/src/**/*.js', 'Frontend/**/*.ejs', 'Frontend/src/*.js'],
+            //Які завдання виконувати під час зміни в файлах
+            tasks: ['browserify:map']
+        }
+    };
+
+
+    //Ініціалузвати Grunt
+    config.watch = watchDebug;
+    grunt.initConfig(config);
+
+    //Сказати які модулі необхідно виокристовувати
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+    //Список завданнь по замовчування
+    grunt.registerTask('default',
+        [
+            'browserify:map'
+        ]
+    );
+
+};
