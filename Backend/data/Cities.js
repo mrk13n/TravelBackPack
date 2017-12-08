f27ec89b3f875cd000767350ed914b3596ac7f04
Nazar <naz466@yandex.ru>
2017-12-08 17:18:55 +0200

    add cities

--- Backend/data/Cities.js
+++ Backend/data/Cities.js
@@ -1,8 +1,48 @@
 var cities = [
     {
         id: 1,
+        city: "Paris",
+        icon: 'assets/images/paris.jpg'
+    },
+    {
+        id: 2,
         city: "New York",
-        country: 'USA'
+        icon: 'assets/images/paris.jpg'
+    },
+    {
+        id: 3,
+        city: "London",
+        icon: 'assets/images/paris.jpg'
+    },
+    {
+        id: 4,
+        city: "Saint Petersburg",
+        icon: 'assets/images/paris.jpg'
+    },
+    {
+        id: 5,
+        city: "Barcelona",
+        icon: 'assets/images/paris.jpg'
+    },
+    {
+        id: 6,
+        city: "Madrid",
+        icon: 'assets/images/paris.jpg'
+    },
+    {
+        id: 7,
+        city: "Paris",
+        icon: 'assets/images/paris.jpg'
+    },
+    {
+        id: 8,
+        city: "New York",
+        icon: 'assets/images/paris.jpg'
+    },
+    {
+        id: 9,
+        city: "London",
+        icon: 'assets/images/paris.jpg'
     }
 ];
 
