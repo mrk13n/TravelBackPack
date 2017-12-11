507d5feafdc2c09a6b3053d07b37176b0c45dc65
vladiklem <lemvlad99@gmail.com>
2017-12-11 14:05:39 +0200

    style

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,4 +1,11 @@
 $(function () {
     var GetCities = require('./Cities/GetCities');
     GetCities.initialiseCities();
+
+    $(".city-scroll").click(function() {
+        $('html,body').animate({
+                scrollTop: $(".heading").offset().top},
+            'slow');
+    });
+
 });
