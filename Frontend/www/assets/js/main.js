c0ac27da795997d6ae05a4d44a8b5dae4ccabac9
naukmari <naukmari@gmail.com>
2017-12-14 03:58:33 +0200

    add aboutUs page, button scrollDown and some new stuff
    add

--- Frontend/www/assets/js/main.js
+++ Frontend/www/assets/js/main.js
@@ -110,6 +110,12 @@ $(function () {
         $('html, body').animate({ scrollTop: $('.greetings').offset().top }, 'slow');
         return false;
     }
+    $(function() {
+        $('a[href*=#]').on('click', function(e) {
+            e.preventDefault();
+            $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+        });
+    });
 
 });
 
