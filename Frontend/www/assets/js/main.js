eb6e42f425a64ac779b66cd14bdde66d858f48ef
Nazar <naz466@yandex.ru>
2018-01-30 21:53:25 +0200

    commit
    Search not full name

--- Frontend/www/assets/js/main.js
+++ Frontend/www/assets/js/main.js
@@ -82,6 +82,7 @@ var Storage = require('../LocalStorage');
 function getId(text) {
     var nameCity;
     var find = false;
+    var id;
     API.getCitiesList(function (err, data) {
         if (!err) {
             Cities = data;
@@ -90,10 +91,29 @@ function getId(text) {
                     text = text.toLowerCase();
                     nameCity = Cities[i].city.toLowerCase();
                     if (text === nameCity) {
-                        var id = Cities[i].id;
+                        id = Cities[i].id;
                         Storage.set('id', id);
-                        document.location.href = '/city.html'
+                        document.location.href = '/city.html';
                         find = true;
+                    }
+                }
+            }
+            if (!find) {
+                for (var j = 0; j < Cities.length; j++) {
+                    if (text !== undefined) {
+                        text = text.toLowerCase();
+                        var a = text.length;
+                        var name = '';
+                        nameCity = Cities[j].city.toLowerCase();
+                        for (var k = 0; k < a; k++) {
+                            name += nameCity[k];
+                        }
+                        if (text === name) {
+                            id = Cities[j].id;
+                            Storage.set('id', id);
+                            document.location.href = '/city.html';
+                            find = true;
+                        }
                     }
                 }
             }
