290e8b494cbd4117d9a75cea8ad6fcd32bda6c47
Nazar <naz466@yandex.ru>
2019-11-28 17:09:05 +0200

    update API

--- Frontend/src/API.js
+++ Frontend/src/API.js
@@ -1,12 +1,11 @@
-// const API_URL = '127.0.0.1:4040';
 async function backendGet(API_URL, callback) {
     await $.ajax({
         url: API_URL,
         type: 'GET',
-        success: function(data){
+        success: (data) => {
             callback(null, data);
         },
-        error: function() {
+        error: () => {
             callback(new Error("Ajax Failed"));
         }
     })
@@ -18,47 +17,47 @@ async function backendPost(API_URL, data, callback) {
         type: 'POST',
         contentType : 'application/json',
         data: JSON.stringify(data),
-        success: function(data){
+        success: (data) => {
             callback(null, data);
         },
-        error: function() {
+        error: () => {
             callback(new Error("Ajax Failed"));
         }
     })
 }
 
-exports.checkLogin = async function (callback) {
+exports.checkLogin = async (callback) => {
     await backendGet('/api/check-login/', callback);
 };
 
-exports.getCitiesList = async function(callback) {
+exports.getCitiesList = async (callback) => {
     await backendGet('/api/get-cities/', callback);
 };
 
-exports.registration = async function (user, callback) {
+exports.registration = async (user, callback) => {
     await backendPost('/api/registration/', user, callback);
 };
 
-exports.login = async function (user, callback) {
+exports.login = async (user, callback) => {
     await backendPost('/api/login/', user, callback);
 };
 
-exports.logout = async function (callback) {
+exports.logout = async (callback) => {
     await backendGet('/api/logout/', callback);
 };
 
-exports.getComments = function (city, callback) {
-  backendPost('/api/get-comments/', city, callback);
+exports.getComments = async (city, callback) => {
+  await backendPost('/api/get-comments/', city, callback);
 };
 
-exports.writeComment = function (comment, callback) {
-  backendPost('/api/write-comments/', comment, callback);
+exports.writeComment = async (comment, callback) => {
+  await backendPost('/api/write-comments/', comment, callback);
 };
 
-exports.getBackpack = function (callback) {
-    backendGet('/api/get-backpack/', callback);
+exports.getBackpack = async (callback) => {
+    await backendGet('/api/get-backpack/', callback);
 };
 
-exports.setBackpack = function (backpack, callback) {
-    backendPost('/api/set-backpack/', backpack, callback);
+exports.setBackpack = async (backpack, callback) => {
+    await backendPost('/api/set-backpack/', backpack, callback);
 };
